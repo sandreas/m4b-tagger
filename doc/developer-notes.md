c957974c08e53099551db8df6dee64e78658f31b
andreas <andreas.schroden@gmail.com>
2017-10-05 19:28:28 +0200

    Restructured gradle build, added first Window for GUI

---
+++ doc/developer-notes.md
@@ -0,0 +1,6 @@
+Interesting Projects:
+https://zeroturnaround.com/rebellabs/best-javafx-libraries-for-beautiful-apps-and-clean-code/
+https://github.com/TestFX/TestFX
+
+Video Tutorials:
+https://www.youtube.com/watch?v=FLkOX4Eez6o
--- src/main/java/de/fynder/m4b_tagger/Main.java
+++ src/main/java/de/fynder/m4b_tagger/Main.java
@@ -1,9 +1,24 @@
 package de.fynder.m4b_tagger;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("m4b_tagger");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("m4b-tagger");
+
+        StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
