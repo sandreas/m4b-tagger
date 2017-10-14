8da4e00bde511f82f0b7109bd586831189389c39
andreas <andreas.schroden@gmail.com>
2017-10-14 09:18:35 +0200

    Added some developer notes, check for system exceutables

--- doc/developer-notes.md
+++ doc/developer-notes.md
@@ -4,3 +4,35 @@ https://github.com/TestFX/TestFX
 
 Video Tutorials:
 https://www.youtube.com/watch?v=FLkOX4Eez6o
+
+
+https://stackoverflow.com/questions/22569046/how-to-make-an-os-x-menubar-in-javafx
+http://docs.oracle.com/javafx/2/ui_controls/menu_controls.htm
+http://docs.oracle.com/javafx/2/layout/builtin_layouts.htm
+http://docs.oracle.com/javafx/2/get_started/form.htm
+
+
+
+FFmpeg ffmpeg = new FFmpeg("/path/to/ffmpeg");
+FFprobe ffprobe = new FFprobe("/path/to/ffprobe");
+
+FFmpegBuilder builder = new FFmpegBuilder()
+    .setInput(in)
+    .overrideOutputFiles(true)
+    .addOutput("output.mp4")
+        .setFormat("mp4")
+        .setTargetSize(250000)
+
+        .disableSubtitle()
+
+        .setAudioChannels(1)
+        .setAudioCodec("libfdk_aac")
+        .setAudioRate(48000)
+        .setAudioBitrate(32768)
+
+        .setVideoCodec("libx264")
+        .setVideoFramerate(Fraction.getFraction(24, 1))
+        .setVideoResolution(640, 480)
+
+        .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
+        .done();
--- src/main/java/de/fynder/m4b_tagger/Main.java
+++ src/main/java/de/fynder/m4b_tagger/Main.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.nio.file.Paths;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -13,12 +15,39 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("m4b-tagger");
+//        primaryStage.setTitle("m4b-tagger");
+//
+//        StackPane layout = new StackPane();
+//
+//        Scene scene = new Scene(layout, 300, 200);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
 
-        StackPane layout = new StackPane();
+        // http://www.mkyong.com/java/java-properties-file-examples/
+//        String homeDir = System.getProperty("user.home");
+//        String configDir = Paths.get(homeDir + "/.m4b-tagger");
 
-        Scene scene = new Scene(layout, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        String executable = "graft";
+
+        if(executableExists(executable)) {
+            System.out.println(executable + " exists");
+        }else {
+            System.out.println(executable + " is missing");
+        }
+
+
+
+        System.exit(0);
+    }
+
+    private boolean executableExists(String executable) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(executable);
+            proc.waitFor();
+            return proc.exitValue() == 0;
+        } catch(Exception e) {
+            return false;
+        }
     }
 }
